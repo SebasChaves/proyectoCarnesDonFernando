d7f73cec75a7ef05e78f326b0b23d15d6a2453b8
Nivek30 <kfonseca40711@ufide.ac.cr>
2023-03-10 18:20:02 -0600

    detalle

---
+++ CarnesDonFernando/FrontEnd/Models/DetalleOrdenViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.Models
+{
+    public class DetalleOrdenViewModel
+    {
+
+        public int IdOrden { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public double Subtotal { get; set; }
+        public double Total { get; set; }
+
+    }
+}
